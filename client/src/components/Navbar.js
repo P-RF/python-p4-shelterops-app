b33e76819bd543ec962a7713b0fc3f8ccdc08670
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2026-03-23 11:29:45 -0400

    Set up React routing, session persistence, and Navbar navigation

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,35 @@
 // Navbar.js
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ user, setUser}) {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    fetch("http://127.0.0.1:5555/logout", {
+      method: "DELETE",
+      credentials: "include"
+    })
+    .then(() => {
+      setUser(null);
+      navigate("/login")
+    });
+  }
+
   return (
     <nav>
-      <Link to="/">Dashboard</Link>
-      <Link to="/pets">Pets</Link>
-      <Link to="/medication_logs">Logs</Link>
+      <h2>ShelterOps</h2>
+      <div>
+        <Link to="/">Dashboard</Link>
+        <Link to="/pets">Pets</Link>
+        <Link to="/medication_logs">Medication Logs</Link>
+      </div>
+      <div>
+        <span>Welcome, {user?.name}</span>
+        <button onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </nav>
   );
 }
